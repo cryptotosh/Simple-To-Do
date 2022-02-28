15f7aafbd2d41a675a25842240a092643eb6e84c
Stephen Omotosho <giftedtosh@gmail.com>
2022-02-28 14:03:37 +0100

    css & js updated

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple-To-Do
+
+Simple To-Do list using JS
+Actually, this is my first task using javascript... will update as soon as I progress in my learning
--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+function showItems(){
+    document.getElementById("itemsAdd").style.display="";
+}
+
+
+
 var form=document.getElementById("addForm");
 var listItem=document.getElementById("list");
+//var para=document.querySelector('#addAlert');
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -16,35 +23,36 @@ form.addEventListener("submit", (e)=>{
     li.appendChild(button);
     listItem.appendChild(li);
     
-    document.getElementById("addAlert").style.display=""
+//    var tBox=document.querySelector('.form-control');
+//    tBox.innerHTML="";
+//    tBox.setAttribute('placeholder', "");
+    var del=document.getElementById('addAlert');
+    del.style.display="";
+
+    del.innerHTML="Item added successfully!";
+    del.style.color='rgb(0, 128, 0)';
+    del.style.borderColor='rgb(0, 128, 0)';
+
 })
 
+
 listItem.addEventListener("click", (e)=>{
+   var del=document.getElementById('addAlert');
     if(e.target.classList.contains("btn-danger")){
-        document.getElementById("delAlert").style.display=""
+        del.innerHTML="Item deleted successfully!";
+        del.style.color='rgb(255, 0, 0)';
+        del.style.borderColor='rgb(255, 0, 0)';
+        
     var li=e.target.parentElement;
     listItem.removeChild(li);
+
     }
 })
 
-function showItems(){
-    document.getElementById("itemsAdd").style.display="";
-   
-
-    
+ 
     
 
     
     
     
     
-//   var input=document.querySelector('input[type="text"]');
-//    if(input.v)
-//   document.getElementById("addAlert").style.display="";
-}
-
-  
-//    var addP=document.createElement("p");
-//    var addT=document.createTextNode("Item added successfully!");
-//    var addR=document.querySelector("#addAlert");
-//    addR.replaceChild(p)
